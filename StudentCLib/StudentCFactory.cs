c430801ddc96fa94eeea834c80cf0488d1b1a04c
Łukasz Brzeziński <neox210@gmail.com>
2019-10-13 12:41:42 +0200

    Add factory for studentC lib

---
+++ StudentCLib/StudentCFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StudentCLib.PunctuationMarkCounter;
+using PunctuationCounter = StudentCLib.PunctuationMarkCounter.PunctuationMarkCounter; 
+
+namespace StudentCLib
+{
+    public static class StudentCFactory
+    {
+        public static IPunctuationMarkCounter PunctuationMarkCounter()
+        {
+            return new PunctuationCounter();
+        }
+    }
+}
