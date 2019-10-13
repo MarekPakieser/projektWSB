3dfdd23aa25595291f8c6f2e1dc3ab5063cb4e80
LukaszBrzezinski <lukasz.grzegorz.brzezinski@gmail.com>
2019-10-13 13:19:02 +0200

    add basic implementation of sentences count

---
+++ StudentCLib/SentencesCounter/ISentencesCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentCLib.SentencesCounter
+{
+    public interface ISentencesCounter
+    {
+        int CountSentences(string input);
+    }
+
+    class SentencesCounter : ISentencesCounter
+    {
+        public int CountSentences(string input)
+        {
+            return input.Count(c => c == '.');
+        }
+    }
+}
